db924892f696e1f5e3e4c0af1ab5487a6dfa7bf4
The-Quantum-Boy <dhanorkar.sumit.code@gmail.com>
2023-04-12 08:46:51 +0530

    java guide tutorial completed

--- src/main/java/com/springdatajpa/entity/Product.java
+++ src/main/java/com/springdatajpa/entity/Product.java
@@ -21,7 +21,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(
@@ -66,6 +65,78 @@ public class Product {
     
     @UpdateTimestamp
     private LocalDateTime lastUpdated;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public LocalDateTime getDataCreated() {
+		return dataCreated;
+	}
+
+	public void setDataCreated(LocalDateTime dataCreated) {
+		this.dataCreated = dataCreated;
+	}
+
+	public LocalDateTime getLastUpdated() {
+		return lastUpdated;
+	}
+
+	public void setLastUpdated(LocalDateTime lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
 
 
 }
